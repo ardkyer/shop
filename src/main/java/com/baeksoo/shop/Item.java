2cdd93c6366fee65991cdff58b9b5fcebfe765c7
ardkyer <k0207k@naver.com>
2024-07-19 16:56:38 +0900

    0719_index_end

--- src/main/java/com/baeksoo/shop/Item.java
+++ src/main/java/com/baeksoo/shop/Item.java
@@ -1,9 +1,6 @@
 package com.baeksoo.shop;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -12,6 +9,7 @@ import lombok.ToString;
 @ToString
 @Getter
 @Setter
+@Table(indexes = @Index(columnList = "title", name = "작명"))
 public class Item {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     public Integer id;
--- src/main/java/com/baeksoo/shop/ItemRepository.java
+++ src/main/java/com/baeksoo/shop/ItemRepository.java
@@ -1,10 +1,16 @@
 package com.baeksoo.shop;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ItemRepository extends JpaRepository<Item, Integer>{
 
     Optional<Item> findById(Long id);
+    List<Item> findAllByTitleContains(String title);
+
+    @Query(value = "SELECT * FROM shop.item WHERE MATCH(title) AGAINST(?1)",  nativeQuery = true)
+    List<Item> rawQuery1(String text);
 }
